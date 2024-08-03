39f0593572a71eb57f6285739e461411db9b645d
samarth3301 <samarplayz1337@gmail.com>
2024-08-03 15:36:43 +0530

    added basic guild join and leave events

--- src/cogs/admin.py
+++ src/cogs/admin.py
@@ -5,11 +5,12 @@ from core.bot import Bot
 
 class AdminCommands(commands.Cog):
     def __init__(self, bot: Bot):
-        super().__init__()
+        self.bot = bot
     
     @commands.group(
         name='pannel'
     )
+    @commands.has_permissions(administrator=True)
     async def pannel(self, ctx: commands.Context):
         embed = discord.Embed(
             color=config.Color.default,
@@ -18,16 +19,39 @@ class AdminCommands(commands.Cog):
         await ctx.reply(embed=embed, mention_author=False)
     
     @pannel.command(
-        name='add'
+        name='create',
+        aliases=['add']
     )
+    @commands.has_permissions(administrator=True)
     async def pannel_add(self, ctx: commands.Context):
         pass
 
     @pannel.command(
-        name='remove'
+        name='delete',
+        aliases=['remove']
     )
+    @commands.has_permissions(administrator=True)
     async def pannel_remove(self, ctx: commands.Context):
         pass
 
+
+    @commands.group(
+        name='config'
+    )
+    async def guild_configurations(self, ctx: commands.Context):
+        guildData = await self.bot.db.guildconfig.find_first(
+            where={
+                'guildId' : ctx.guild.id
+            }
+        )
+        embed = discord.Embed(
+            color=config.Color.default,
+            description=
+            f'> prefix : {guildData.prefix}\n'
+            f'> pannels: {guildData.pannel_count}'
+        )
+        embed.set_author(name=f'{ctx.guild.name}\'s config')
+        await ctx.reply(embed=embed, view=None)
+    
 async def setup(bot: Bot):
     await bot.add_cog(AdminCommands(bot))
--- src/cogs/events.py
+++ src/cogs/events.py
@@ -1,15 +1,46 @@
 import discord
 from discord.ext import commands
 from core.bot import Bot
+from utils.logger import logger
 
 
 class GlobalEvents(commands.Cog):
     def __init__(self, bot: Bot):
-        super().__init__()
+        self.bot = bot
 
-    @commands.Cog.listener('on_guild_join')
-    async def guild_join_event(self, ctx: commands.Context):
-        pass
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild: discord.Guild):
+        logger.info(f"Guild joined: {guild.id}")
+        try:
+            guild_check = await self.bot.db.guildconfig.find_unique(where={
+                'guildId': int(guild.id),  
+            })
+            if not guild_check:
+                try:
+                    await self.bot.db.guildconfig.create(data={
+                        'guildId': int(guild.id),
+                    })
+                except Exception as e:
+                    logger.error(f"Error adding guild configuration to the database: {e}")
+        except Exception as e:
+            logger.error(f"Error querying guild configuration: {e}")
+
+    @commands.Cog.listener()
+    async def on_guild_leave(self, guild: discord.Guild):
+        logger.info(f"Guild left: {guild.id}")
+        try:
+            guild_check = await self.bot.db.guildconfig.find_first(where={
+                'guildId': int(guild.id),
+            })
+            if guild_check:
+                try:
+                    await self.bot.db.guildconfig.delete(where={
+                        'guildId': int(guild.id)
+                    })
+                except Exception as e:
+                    logger.error(f"Error removing guild configuration from the database: {e}")
+        except Exception as e:
+            logger.error(f"Error querying guild configuration: {e}")
 
 async def setup(bot: Bot):
     await bot.add_cog(GlobalEvents(bot))
