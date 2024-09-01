c56daac42e658704a5fdfa1cd08a7b571419b32e
Samarth <samarplayz1337@gmail.com>
2024-09-01 14:11:13 +0530

    added tickets counter in db made minor changes in utils commands

--- src/cogs/utils.py
+++ src/cogs/utils.py
@@ -11,7 +11,7 @@ class UtilCommands(commands.Cog):
         name='ping'
     )
     async def ping(self, ctx: commands.Context):
-        await ctx.send(f'🏓 Pong! {round(self.bot.latency * 1000)}ms')
+        await ctx.reply(f'🏓 Pong! {round(self.bot.latency * 1000)}ms', mention_author=False)
 
     @commands.command(
         name='links',
@@ -21,19 +21,19 @@ class UtilCommands(commands.Cog):
         embed = discord.Embed(
             color=self.bot.config.Color.default,
             description=
-            'Hi, I am Qwenzie.\n'
+            '👋 Hi, I am **Qwenzie**.\n\n'
             f'A adorable discord bot. Capable to manage your servers with ease.\n'
-            f'You can invite me to your server by clicking on the Invite button below.\n'
-            f'If you need any help or support, you can join my support server by clicking on the Support Server button below.'
-            f'If you have any suggestions or feedback, you can join my support server and share your thoughts.'
+            f'You can invite me to your server by clicking on the Invite button below.\n\n'
+            f'`➡️`If you need any help or support, you can join my support server by clicking on the Support Server button below.\n'
+            f'`➡️` If you have any suggestions or feedback, you can join my support server and share your thoughts.'
         )
         embed.set_footer(text=f'requested by {ctx.author.name}', icon_url=ctx.author.display_avatar)
-        embed.set_author(name='Qwenzie', icon_url=self.bot.user.avatar_url)
+        embed.set_author(name='Qwenzie', icon_url=self.bot.user.display_avatar)
         view = discord.ui.View()
         view.add_item(discord.ui.Button(label='Support Server', url=self.bot.config.Links.support, style=discord.ButtonStyle.link))
         view.add_item(discord.ui.Button(label='Invite Me', url=self.bot.config.Links.invite, style=discord.ButtonStyle.link))
-        await ctx.reply(view=view, embed=embed)
+        await ctx.reply(view=view, embed=embed, mention_author=False)
 
 
 async def setup(bot: Bot):
-    bot.add_cog(UtilCommands(bot))
+    await bot.add_cog(UtilCommands(bot))
